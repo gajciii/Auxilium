6c24a2e36e13bab86a401ba19c7af1ac9c816e66
Tevž Starovasnik <tevzstarovasnik@Tevzs-Air.domain_not_set.invalid>
2024-01-07 22:07:04 +0100

    mala spremembica (mail)“

---
+++ Auxilium/SpringBootVzpostavitev/src/main/java/si/um/feri/ris/EmailSender/EmailSendingService.java
@@ -0,0 +1,9 @@
+package si.um.feri.ris.EmailSender;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSendingService {
+
+}

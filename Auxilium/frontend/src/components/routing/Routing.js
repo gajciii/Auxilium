17d657e52ce9e9ed577838f685c43f8ecf0bb5a6
Tevž Starovasnik <tevzstarovasnik@Tevzs-MacBook-Air.local>
2024-01-23 20:26:13 +0100

    Kreacija reacta + routing (osnova)

---
+++ Auxilium/frontend/src/components/routing/Routing.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Route } from "react-router";
+import { Routes } from "react-router-dom";
+
+export default function Routing(){
+    return(
+        <Routes>
+            
+        </Routes>
+    );
+}
